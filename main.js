8a2b92e34d06ad5289a1b3fcaad36d8010fc7bba
Ying Gu <guying005@googlemail.com>
2022-12-09 04:40:22 -0300

    home

--- main.js
+++ main.js
@@ -3,6 +3,28 @@
 import Vue from 'vue'
 import App from './App'
 
+import {$http} from '@escook/request-miniprogram'
+uni.$http =$http
+$http.baseUrl ='https://api-ugo-web.itheima.net'
+
+$http.beforeRequest=function(options){
+	uni.showLoading({
+		title: "数据加载中。。。"
+	})
+}
+$http.afterRequest=function(){
+	uni.hideLoading()
+}
+
+// 封装的展示消息提示的方法
+uni.$showMsg = function (title = '数据加载失败！', duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none',
+  })
+}
+
 Vue.config.productionTip = false
 
 App.mpType = 'app'
@@ -12,6 +34,7 @@ const app = new Vue({
 })
 app.$mount()
 // #endif
+
 
 // #ifdef VUE3
 import { createSSRApp } from 'vue'
